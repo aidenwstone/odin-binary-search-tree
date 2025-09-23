81f29e6eef3c1af8a183248d2198e67093bdcd71
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-09-23 11:12:19 -0400

    Node: Add left_child & right_child default values

--- lib/node.rb
+++ lib/node.rb
@@ -8,7 +8,7 @@ class Node
 
   attr_accessor :value, :left_child, :right_child
 
-  def initialize(value, left_child, right_child)
+  def initialize(value, left_child = nil, right_child = nil)
     @value = value
     @left_child = left_child
     @right_child = right_child
